40c861c0e96a60dc09ed8da9b9e702a34bcc9bc7
wolong666 <bxd.kstn@gmail.com>
2023-04-13 17:52:37 +0700

    bai3

--- bai2/bai2.js
+++ bai2/bai2.js
@@ -1,1 +1,7 @@
 console.log('Ban chac chan muon load lai trang')
+let x = 5, y =6, z =7;
+console.log("De bai la: ", 'x=', x,'y=', y, 'z=' , z)
+let a = 'Tôi là 1 string chuỗi'
+console.log(a)
+// console.log(4+5+a)
+// console.log(a+4+5+a)
